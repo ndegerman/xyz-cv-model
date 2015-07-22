7b2391f66dfc8825f9e2d24b8863933724b52d4b
Anton Lundin <anton.lundin@softhouse.se>
2015-07-22 09:19:21 +0200

    added assignments to model

---
+++ app/resource/userToAssignmentConnector.resource.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var request = require('request-promise');
+var config = require('config');
+var responseHandler = require('../utils/response.handler');
+var errorHandler = require('../utils/error.handler');
+
+var url = config.API_URL + 'userToAssignmentConnector';
+
+exports.getUserToAssignmentConnectorsByAssignmentId = function(id, headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url + '/assignment/' + id,
+        method: 'GET',
+        headers: headers
+    };
+
+    return request(options)
+        .then(responseHandler.parseGetPolyQuery)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
+
+exports.getUserToAssignmentConnectorsByUserId = function(id, headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url + '/user/' + id,
+        method: 'GET',
+        headers: headers
+    };
+
+    return request(options)
+        .then(responseHandler.parseGetPolyQuery)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
+
+exports.getAllUserToAssignmentConnectors = function(headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url,
+        method: 'GET',
+        headers: headers
+    };
+
+    return request(options)
+        .then(responseHandler.parseGetPolyQuery)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
