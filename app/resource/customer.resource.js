468213887ccf8ec95a3d1a208899b4d1163f5af7
Rasmus Letterkrantz <rasmus.letterkrantz@gmail.com>
2015-10-28 14:12:15 +0100

    added domain and customer to the assignments

---
+++ app/resource/customer.resource.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var request = require('request-promise');
+var config = require('config');
+var Promise = require('bluebird');
+var responseHandler = require('../utils/response.handler');
+var errorHandler = require('../utils/error.handler');
+
+var url = config.API_URL + 'customer';
+
+exports.getCustomerById = function(id, headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url + '/' + id,
+        method: 'GET',
+        json: true,
+        headers: headers,
+        gzip: true
+    };
+
+    return request(options)
+        .then(responseHandler.parseGet)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
+
+exports.getAllCustomers = function(headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url,
+        method: 'GET',
+        headers: headers,
+        gzip: true
+    };
+
+    return request(options)
+        .then(responseHandler.parseGetPolyQuery)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
+
