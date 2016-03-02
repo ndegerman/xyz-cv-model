047c6969c73377435f3addd4175c5c9d751ccd75
ndegerman <ndegerman@gmail.com>
2016-03-02 14:27:04 +0100

    Added other and userToOther resources and load methods in profile.model

---
+++ app/resource/other.resource.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var request = require('request-promise');
+var config = require('config');
+var responseHandler = require('../utils/response.handler');
+var errorHandler = require('../utils/error.handler');
+
+var url = config.API_URL + 'other';
+
+exports.getOtherById = function(id, headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url + '/' + id,
+        method: 'GET',
+        json: true,
+        headers: headers,
+        gzip: true
+    };
+
+    return request(options)
+        .then(responseHandler.parseGet)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
+
+exports.getAllOthers = function(headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url,
+        method: 'GET',
+        headers: headers,
+        gzip: true
+    };
+
+    return request(options)
+        .then(responseHandler.parseGetPolyQuery)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
+
+exports.getOtherByName = function(name, headers) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: url + '/query?name=' + name,
+        method: 'GET',
+        headers: headers,
+        gzip: true
+    };
+
+    return request(options)
+        .then(responseHandler.parseGetMonoQuery)
+        .catch(errorHandler.throwDREAMSHttpError);
+};
